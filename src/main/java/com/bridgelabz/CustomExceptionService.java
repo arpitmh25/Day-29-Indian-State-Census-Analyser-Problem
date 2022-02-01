fbb5d9abd49f5d3fee158435b27a3626f5c2d177
Arpit <arpitmh23@gmail.com>
2022-02-01 23:03:55 +0530

    Implementing Use cases in Census Analyzer Problem

---
+++ src/main/java/com/bridgelabz/CustomExceptionService.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class CustomExceptionService extends RuntimeException {
+    public enum ExceptionType {
+        FILE_NOT_FOUND,
+        WRONG_FILE_TYPE,
+        WRONG_HEADER
+    }
+
+    public ExceptionType type;
+
+    public CustomExceptionService(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
+}
